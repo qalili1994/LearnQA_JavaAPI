4972fbc41d367df3d6f5bb66ee3cd25074c4d132
qalili1994 <livlad1994@gmail.com>
2022-12-15 23:38:42 +0800

    Ex.10

--- src/test/java/api/AssertHomeWork.java
+++ src/test/java/api/AssertHomeWork.java
@@ -1,16 +1,10 @@
 package api;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AssertHomeWork {
-
     @ParameterizedTest
     @ValueSource(strings= {"qqqqqqqqqqqqqqqqq", "qwwwwwwwwqq"})
     public void test(String strings) {

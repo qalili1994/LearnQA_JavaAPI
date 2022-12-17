a8c32fef10baebd0913f04db82a33ee1b8b483fa
qalili1994 <livlad1994@gmail.com>
2022-12-18 01:29:47 +0800

    Ex.12

--- src/test/java/tests/HomeWorkUserAuthTests.java
+++ src/test/java/tests/HomeWorkUserAuthTests.java
@@ -1,18 +1,16 @@
 package tests;
 
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import java.util.Map;
-
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeWorkUserAuthTests {
 
     @Test
-
     public void getCookie() {
-
         Response responseGetCookies = given()
                 .when()
                 .get("https://playground.learnqa.ru/api/homework_cookie")
@@ -23,5 +21,17 @@ public class HomeWorkUserAuthTests {
 
         System.out.println(responseGetCookies.getCookie("HomeWork"));
         assertTrue(cookies.containsKey("HomeWork"),"Response without 'HomeWork' cookie");
+    }
+
+    @Test
+    public void getHeader() {
+        Response responseGetHeader = given()
+                .when()
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+       // String header = responseGetHeader.getHeader("x-secret-homework-header");
+        Headers headers = responseGetHeader.getHeaders();
+        assertTrue(headers.hasHeaderWithName("x-secret-homework-header"), "Response without 'x-secret-homework-header' header");
     }
 }

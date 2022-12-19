963e67c8620e192db7a78066ca3cce9e3de81e4a
qalili1994 <livlad1994@gmail.com>
2022-12-19 12:54:41 +0800

    Ex.10

--- src/test/java/api/AssertHomeWork.java
+++ src/test/java/api/AssertHomeWork.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AssertHomeWork {
     @ParameterizedTest
-    @ValueSource(strings= {"qqqqqqqqqqqqqqqqq", "qwwwwwwwwqq"})
+    @ValueSource(strings= {"qqqqqqqqqqqqqqqqq", "qwwwwwwwqq"})
     public void test(String strings) {
         assertTrue(strings.length() > 15, "length < 15 symbol");
     }
